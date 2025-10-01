b838648055a39869b822d74ec020273156707dbe
wzxxing <169175349+wzxxing@users.noreply.github.com>
2025-10-01 23:24:06 +0200

    chore: separate unit test and integ tests using pytest markers (#21)
    * separate unit test and integ tests in different directories
    
    * select only unit test in python.yml workflow
    
    * add doc string to test

---
+++ tests/integ/test_hello_world.py
@@ -0,0 +1,6 @@
+def test_hello_world():
+    """This is a placeholder test case.
+
+    To prove that unit tests are selected by the python workflow.
+    """
+    raise ValueError('Should not happen in unit test')
