d4444eefb697332d3d74987a9911f57ee6f1622f
Weizhou Xing <169175349+wzxxing@users.noreply.github.com>
2025-10-01 22:11:55 +0200

    select only unit test in python.yml workflow

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,10 @@
-import pathlib
 import pytest
 
 
 def pytest_collection_modifyitems(config, items):
     """Mark the entire module as unit."""
-    rootdir = pathlib.Path(config.rootdir)
     for item in items:
-        rel_path = pathlib.Path(item.fspath).relative_to(rootdir)
-        if 'unit' in rel_path.parts:
+        if 'unit' in item.nodeid:
             item.add_marker(pytest.mark.unit)
-        elif 'integ' in rel_path.parts:
+        elif 'integ' in item.nodeid:
             item.add_marker(pytest.mark.integ)
