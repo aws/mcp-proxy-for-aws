fb9afeb828522557419d4002992bb3edce5aea22
Weizhou Xing <169175349+wzxxing@users.noreply.github.com>
2025-12-04 14:55:44 +0100

    fix: do not call initialize for q dev cli / kiro cli

--- mcp_proxy_for_aws/middleware/initialize_middleware.py
+++ mcp_proxy_for_aws/middleware/initialize_middleware.py
@@ -28,7 +28,17 @@ class InitializeMiddleware(Middleware):
             client = await self._client_factory.get_client()
             # connect the http client, fail and don't succeed the stdio connect
             # if remote client cannot be connected
-            await client._connect()
+            client_name = context.message.params.clientInfo.name.lower()
+            if 'kiro cli' not in client_name and 'q dev cli' not in client_name:
+                # q cli / kiro cli uses the rust SDK which does not handle json rpc error
+                # properly during initialization.
+                # https://github.com/modelcontextprotocol/rust-sdk/pull/569
+                # if calling _connect below raise mcp error, the q cli will skip the message
+                # and continue wait for a json rpc response message which will never come.
+                # Luckily, q cli calls list tool immediately after being connected to a mcp server
+                # the list_tool call will require the client to be connected again, so the mcp error
+                # will be displayed in the q cli logs.
+                await client._connect()
             return await call_next(context)
         except Exception:
             logger.exception('Initialize failed in middleware.')
